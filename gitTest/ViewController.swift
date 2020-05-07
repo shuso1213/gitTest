95ff6aab366d7c63865f106eb37318884eb66c05
shuso1213 <shuso1213@gmail.com>
2020-05-07 21:30:13 +0900

    変数追加

--- gitTest/ViewController.swift
+++ gitTest/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let test = ""
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
